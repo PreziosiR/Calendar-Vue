c95a39cdbb2b49a6c8213661f96629b0cc333ce8
PREZIOSIR <rpreziosi00@gmail.com>
2018-11-14 10:35:32 +0100

    first commit

---
+++ Vue/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="calendar">
+        <div id="calendar_header">
+            <i class="fas fa-chevron-left"></i>
+            <h1>{{ month }}</h1>
+            <i class="fas fa-chevron-right"></i>
+        </div>
+        <div class="content"></div>
+    </div>
+</template>
+
+<script>
+import moment from 'moment'
+
+export default {
+    name: "App",
+    data: () => {
+        return {
+            month: moment().format('MMMM')
+        }
+    }
+}
+</script>
+
